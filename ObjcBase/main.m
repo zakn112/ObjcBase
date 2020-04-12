883f27d80e32c2d5ef878e0905a45b335ce23b99
zakn112 <56603472+zakn112@users.noreply.github.com>
2020-04-12 12:28:21 +0300

    Initial commit

---
+++ ObjcBase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjcBase
+//
+//  Created by Андрей Закусов on 12.04.2020.
+//  Copyright © 2020 Андрей Закусов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
